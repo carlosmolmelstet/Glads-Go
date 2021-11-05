31d2b832ba84217dbcb9e7bd1ed0c12c73880b8a
Carlos Molmelstet <molmelstet2016@gmail.com>
2021-11-04 22:09:25 -0300

    Add production APP url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+
+let development = process.env.NODE_ENV !== 'production';
+
 export const api = axios.create({
-    baseURL: 'https://api-glads-go.herokuapp.com/'
-});
+    baseURL: development ?  "https://localhost:44395/" : "https://api-glads-go.herokuapp.com/"
+})
